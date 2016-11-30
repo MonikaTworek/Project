a3bc999f3849624b5f59e7bc886bda8c543d4e8d
Mona <montwor@wp.pl>
2016-11-30 20:11:51 +0100

    Zarys klas klient-serwer

---
+++ src/main/java/Client.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Mona on 30.11.2016.
+ */
+public class Client {
+    int dim;
+    PlayerColor ccolor;
+
+    Client(int dim, PlayerColor ccolor){
+        this.ccolor=ccolor;
+        this.dim=dim;
+    }
+}
